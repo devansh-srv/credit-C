d200f07616142a14c98fe20bd7fa2e4d49c78b89
Devansh Srivastava <devansh.srv@gmail.com>
2024-09-21 12:40:07 +0530

    main app created

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from config import Config
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+CORS(app)
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+
+from routes.auth import auth_bp
+from routes.carbon import carbon_bp
+from routes.transaction import transaction_bp
+
+app.register_blueprint(auth_bp, url_prefix = '/api/auth')
+app.register_blueprint(carbon_bp, url_prefix = '/api/carbon')
+app.register_blueprint(transaction_bp, url_prefix = '/api/transaction')
+
+if __name__ == '__main__':
+    app.run(debug=True)
