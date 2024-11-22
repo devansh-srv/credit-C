6c008e267d012aa36052849e8b1a0d74a74d169a
Devansh Srivastava <devansh.srv@gmail.com>
2024-11-23 00:11:57 +0530

    fixed amount

--- backend/app/models/credit.py
+++ backend/app/models/credit.py
@@ -6,3 +6,4 @@ class Credit(db.Model):
     name = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=False)
+    is_active = db.Column(db.Boolean,default = True)
