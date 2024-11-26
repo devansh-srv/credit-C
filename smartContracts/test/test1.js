0b37af03d2d5bf765c9717a17179efc75df2babd
Harshit <harshitjaiswal2606@gmail.com>
2024-11-26 15:11:23 +0530

    updated smart contract

--- smartContracts/test/test1.js
+++ smartContracts/test/test1.js
@@ -4,12 +4,12 @@ const { ethers } = require("hardhat");
 
 describe("Carbon Credit", function(){
   async function deployFixture() {
-    const [acc1, acc2] = await ethers.getSigners();
+    const [acc1, acc2, acc3] = await ethers.getSigners();
     // const Contract = await ethers.getContractFactory("SimpleCarbonCredit");
     const carbonCredit = await ethers.deployContract("SimpleCarbonCredit");
     await carbonCredit.waitForDeployment();
 
-    return {carbonCredit, acc1, acc2};
+    return {carbonCredit, acc1, acc2, acc3};
   }
 
   it("Should generate a Carbon Credit", async function () {
@@ -49,7 +49,7 @@ describe("Carbon Credit", function(){
   })
 
   it("should sell to another account", async function () {
-    const {carbonCredit, acc1, acc2} = await loadFixture(deployFixture);
+    const {carbonCredit, acc1, acc2, acc3} = await loadFixture(deployFixture);
 
     const amount = 10;
     const creditId = 0;
@@ -66,9 +66,33 @@ describe("Carbon Credit", function(){
     //acc2 buying it 
     await expect(() => carbonCredit.connect(acc2).buyCredit(creditId, {value: buyPrice})
       ).to.changeEtherBalances([acc2, acc1],[-price, price]);
-    
+
     credit = await carbonCredit.credits(0);
     expect(credit.owner).to.equal(acc2.address);
+    expect(credit.forSale).to.be.false;
+
+
+    //fnc for to increase the price and sell to acc3 to buy it and then check if acc1 (creator) gets 10 %
+    new_price = 2;
+    await carbonCredit.connect(acc2).sellCredit(creditId, new_price);
+    credit = await carbonCredit.credits(0); 
+
+    expect(credit.owner).to.equal(acc2.address);
+    expect(credit.forSale).to.be.true;
+    
+    const newBuyPrice = ethers.parseUnits("2","wei")
+
+    // acc3 buys the credit, and acc1 (creator) gets 10%
+    const creatorShare = Math.floor((new_price * 10) / 100); // 10% to creator
+    const ownerShare = new_price - creatorShare; // Remaining to the current owner (acc2)
+
+    await expect(() =>
+        carbonCredit.connect(acc3).buyCredit(creditId, {value: newBuyPrice})
+    ).to.changeEtherBalances([acc3, acc2, acc1], [-new_price, ownerShare, creatorShare]);
+
+    credit = await carbonCredit.credits(0);
+    expect(credit.owner).to.equal(acc3.address);
+    expect(credit.creator).to.equal(acc1.address);
     expect(credit.forSale).to.be.false;
   })
 });
